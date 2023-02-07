3b2c8fefc2a57ee6086e86496a4bdff5883d209b
jargohillzz <chukwunonsougwu14@gmail.com>
2023-02-07 15:13:48 +0100

    first commit

---
+++ src/Hero/index.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import "./Hero.css";
+import heroSVG from "../assets/heroSvg.svg";
+import serverSVG from "../assets/server.svg";
+import locationSVG from "../assets/location.svg";
+import userSVG from "../assets/user.svg";
+
+const Hero = () => {
+  const heroIcons = [
+    {
+      id: 1,
+      image: userSVG,
+      text: "users",
+      num: "90+",
+    },
+    {
+      id: 2,
+      image: locationSVG,
+      text: "locations",
+      num: "30+",
+    },
+    {
+      id: 3,
+      image: serverSVG,
+      text: "servers",
+      num: "50+",
+    },
+  ];
+  return (
+    <div className="hero">
+      <div className="hero-cont">
+        <div className="hero-cont-left">
+          <h1 className="hero-h1">We Are Everything We Aspire To BE</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ac
+            orci condimentum lectus efficitur tempor vel eu massa. Sed molestie
+            turpis eget ipsum malesuada dignissim
+          </p>
+          <button className="heroBtn">get started</button>
+        </div>
+        <div className="hero-cont-right">
+          <div className="hero-img">
+            <img src={heroSVG} alt="hero image" />
+          </div>
+        </div>
+      </div>
+      <div className="hero-icons">
+        {heroIcons.map((heroIcon) => {
+          const { id, image, text, num } = heroIcon;
+          return (
+            <div className="hero-icon" key={id}>
+              <div className="hero-icon-img">
+                <img src={image} alt="hero icons" />
+              </div>
+              <div className="hero-icon-text">
+                <h2>{num}</h2>
+                <p>{text} </p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ src/Navbar/index.jsx
@@ -0,0 +1,74 @@
+import React, { useEffect, useState } from "react";
+import { useRef } from "react";
+import "./navbar.css";
+
+const Navbar = () => {
+  const navRef = useRef();
+  const [isLock, setIsLock] = useState(document.body.style.overflow === "auto");
+  const handleToggle = (e) => {
+    navRef.current.classList.toggle("show-links");
+    setIsLock(!isLock);
+  };
+  useEffect(() => {
+    document.body.style.overflow = isLock ? "hidden" : "auto";
+  });
+
+  const links = [
+    {
+      id: 1,
+      url: "/",
+      text: "About",
+    },
+    {
+      id: 2,
+      url: "/",
+      text: "FAQs",
+    },
+    {
+      id: 3,
+      url: "/",
+      text: "Pricing",
+    },
+    {
+      id: 4,
+      url: "/",
+      text: "Testimonials",
+    },
+  ];
+  return (
+    <div className="navbar">
+      <div className="nav-container">
+        <div className="nav-header">
+          <div className="nav-logo">
+            <h2 className="logo">PEDDLE</h2>
+          </div>
+          <button class="nav-toggle" onClick={handleToggle}>
+            <i class="fas fa-bars"></i>
+          </button>
+        </div>
+        <div className="nav-links">
+          <div className="links-container" ref={navRef}>
+            <div className="links">
+              {links.map((link) => {
+                const { id, url, text } = link;
+                return (
+                  <p key={id}>
+                    <a href={url} className="link">
+                      {text}
+                    </a>
+                  </p>
+                );
+              })}
+            </div>
+            <div className="button-container">
+              <button className="btn">Sign In</button>
+              <button className="btn-outline">Sign Up</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
