6e8c5c472fe821cce56e0cbc4bb32fd5005b53dd
jargohillzz <chukwunonsougwu14@gmail.com>
2023-02-08 06:52:26 +0100

    progress: upto the plan section

--- src/Content/index.jsx
+++ src/Content/index.jsx
@@ -1,8 +1,156 @@
 import React from "react";
 import "./Content.css";
+import provideSVG from "../assets/provideImg.svg";
+import freeSVG from "../assets/freeSVG.svg";
+import standardSVG from "../assets/standardSVG.svg";
+import premiumSVG from "../assets/premiumSVG.svg";
 
 const Content = () => {
-  return <div>Content</div>;
+  return (
+    <div className="content">
+      <section className="provide-section">
+        <div className="provide-img">
+          <img src={provideSVG} alt="provide art" />
+        </div>
+        <div className="provide-text">
+          <h2 className="provide-h2">Features We Provide For You</h2>
+          <p className="provide-p">
+            Curabitur tempor justo non turpis malesuada cursus. Mauris ac libero
+            eu sem finibus lacinia nec pulvinar.
+          </p>
+          <div className="provide-pointers">
+            <div className="provide-point">
+              <div className="provide-box"></div>
+              <p>Curabitur tempor justo</p>
+            </div>
+            <div className="provide-point">
+              <div className="provide-box"></div>
+              <p>Curabitur tempor justo</p>
+            </div>
+            <div className="provide-point">
+              <div className="provide-box"></div>
+              <p>Curabitur tempor justo</p>
+            </div>
+            <div className="provide-point">
+              <div className="provide-box"></div>
+              <p>Curabitur tempor justo</p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="plan-section">
+        <h2>choose your plan</h2>
+        <p>
+          Let's choose the package that is best for you and explore it happily
+          and cheerfully.
+        </p>
+        <div className="plans">
+          <div className="plan">
+            <div className="plan-top">
+              <div className="plan-img">
+                <img src={freeSVG} alt="free plan" />
+              </div>
+              <h3 className="plan-name">free plan</h3>
+              <p>
+                <i class="fa-solid fa-check check"></i>
+                Mauris sem neque
+              </p>
+              <p>
+                <i class="fa-solid fa-check check"></i>
+                Mauris sem neque
+              </p>
+              <p>
+                <i class="fa-solid fa-check check"></i>
+                Mauris sem neque
+              </p>
+              <p>
+                <i class="fa-solid fa-check check"></i>
+                Mauris sem neque
+              </p>
+            </div>
+            <div className="plan-bottom">
+              <h2>
+                <span className="plan-amount">FREE</span>
+              </h2>
+              <button className="selectBtn">select</button>
+            </div>
+          </div>
+          <div className="plan">
+            <div className="plan-top">
+              <div className="plan-img">
+                <img src={standardSVG} alt="standard plan" />
+              </div>
+              <h3 className="plan-name">standard plan</h3>
+              <p>
+                <i class="fa-solid fa-check check"></i>
+                Mauris sem neque
+              </p>
+              <p>
+                <i class="fa-solid fa-check check"></i>
+                Mauris sem neque
+              </p>
+              <p>
+                <i class="fa-solid fa-check check"></i>
+                Mauris sem neque
+              </p>
+              <p>
+                <i class="fa-solid fa-check check"></i>
+                Mauris sem neque
+              </p>
+              <p>
+                <i class="fa-solid fa-check check"></i>
+                Mauris sem neque
+              </p>
+            </div>
+            <div className="plan-bottom">
+              <h2>
+                <span className="plan-amount">$9</span> / mo
+              </h2>
+              <button className="selectBtn">select</button>
+            </div>
+          </div>
+          <div className="plan">
+            <div className="plan-top">
+              <div className="plan-img">
+                <img src={premiumSVG} alt="premium plan" />
+              </div>
+              <h3 className="plan-name">premium plan</h3>
+              <p>
+                <i class="fa-solid fa-check check"></i>
+                Mauris sem neque
+              </p>
+              <p>
+                <i class="fa-solid fa-check check"></i>
+                Mauris sem neque
+              </p>
+              <p>
+                <i class="fa-solid fa-check check"></i>
+                Mauris sem neque
+              </p>
+              <p>
+                <i class="fa-solid fa-check check"></i>
+                Mauris sem neque
+              </p>
+              <p>
+                <i class="fa-solid fa-check check"></i>
+                Mauris sem neque
+              </p>
+              <p>
+                <i class="fa-solid fa-check check"></i>
+                Mauris sem neque
+              </p>
+            </div>
+            <div className="plan-bottom">
+              <h2>
+                <span className="plan-amount">$12</span> / mo
+              </h2>
+              <button className="selectBtn">select</button>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Content;
